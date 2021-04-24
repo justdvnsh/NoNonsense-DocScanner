bb6f2eef9c081b8670d0c12696b4ae9b731ec5f5
Divyansh Dwivedi <justdvnsh2208@gmail.com>
2021-04-24 20:18:44 +0530

    added opencv

--- app/src/main/java/divyansh/tech/nononsense_docscanner/MainActivity.kt
+++ app/src/main/java/divyansh/tech/nononsense_docscanner/MainActivity.kt
@@ -2,10 +2,12 @@ package divyansh.tech.nononsense_docscanner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.opencv.android.OpenCVLoader
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        OpenCVLoader.initDebug()
     }
 }
